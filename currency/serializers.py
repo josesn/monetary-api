3b6eb10333312d9ef5a17fa7e62ddf4dc2346d9b
jose_sn <zeneto1@gmail.com>
2024-03-04 22:52:08 -0300

    refactor: refatorando serializer para retorno do endpoint currency

--- currency/serializers.py
+++ currency/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 
 from .models import ExchargeRate
 
-class ExchargeRateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ExchargeRate
-        fields = '__all__'
+class ConversionSerializer(serializers.Serializer):
+    currency_from = serializers.CharField(max_length=6)
+    currency_to = serializers.CharField(max_length=6)
+    bid = serializers.DecimalField(max_digits=16, decimal_places=5)
+    amount = serializers.DecimalField(max_digits=16, decimal_places=5)
+    conversion_amount = serializers.DecimalField(max_digits=16, decimal_places=5)
